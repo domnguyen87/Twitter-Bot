979f15b7a93bc9148deb1c3d72b435cb558fdf18
Dominique Nguyen <domnguyen@gmail.com>
2019-03-31 22:06:39 -0700

    Twitter Node

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+    consumer_key: '1JlKFKmWDNz8EoWJXcjVbFeL4',
+    consumer_secret: 'l3NNSkGBvOgScUtB2BqjbJSADlv3LEGsAXcZP5fGGFPQtp8X9o',
+    access_token_key: '979853178417147904-oUq7JLadEtNdtyvyMICXq7Whf2A1hUc',
+    access_token_secret: 'P4eIagkTuFFzrWXaiJ1g9QoMyn6KC3tgbGbGdRuFomsvl'
+}
